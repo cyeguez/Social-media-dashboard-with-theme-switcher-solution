03ab7773021b0fb04c1e33d314f4686fef86a0e9
Cesar Yeguez <Cesar.Yeguez@gmail.com>
2024-05-12 20:13:49 -0400

    Se añade el proyecto  con sus estilos y funcionando

---
+++ README-template.md
@@ -0,0 +1,58 @@
+# Frontend Mentor - Social media dashboard with theme switcher solution
+
+This is a solution to the [Social media dashboard with theme switcher challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+Es un reto tomado en la pagina de frontendmentor, el cual consiste en hacer un dasboard donde se muestre las actividades de los usuario
+It is a challenge taken on the frontendmentor page, which consists of making a dashboard where the user's activities are shown.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Toggle color theme to their preference
+
+### Screenshot
+
+![Mobile](./assets/images/screenshot/mobile.png)
+![Mobile](./assets/images/screenshot/desktop.png)
+
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+1. I start by defining the semantic tags of the project.
+2. Define the variables.
+3. Write CSS styles.
+4. Develop logic in CSS.
+
+
+### Built with
+
+[![Tecnology](https://skillicons.dev/icons?i=js,html,css,tailwind, figma)](https://skillicons.dev)
+
+## Author
+
+- Frontend Mentor - [@Cesar Yeguez](https://www.frontendmentor.io/profile/cyeguez)
---
+++ assets/js/app.js
@@ -0,0 +1,126 @@
+const mainFollower = document.querySelector(".main__follower");
+
+const overviewContainer = document.querySelector(".overview__container");
+
+let dataOverview = "";
+const url = "./assets/data/data.json";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    dataOverview = data.overview;
+    render(data.overview);
+    renderOverview(data["overview-today"]);
+  })
+  .catch((error) => {
+    console.log("ha ocurrido un error: " + error);
+  });
+
+function render(data) {
+  data.map(function (item) {
+    let stateText = document.querySelector(".state__text");
+    let nameIcon = "";
+
+    if (item.isUp) {
+      nameIcon = "icon-up.svg";
+    } else {
+      nameIcon = "icon-down.svg";
+    }
+    let bordeColor;
+    let color = item.network;
+
+    switch (color) {
+      case "Facebook":
+        bordeColor = "Facebook";
+        break;
+      case "Twitter":
+        bordeColor = "Twitter";
+        break;
+      case "Instagram":
+        bordeColor = "Instagram";
+        break;
+      case "YouTube":
+        bordeColor = "YouTube";
+        break;
+    }
+
+    const templateCardFollower = `
+    
+    <div class="follower__card ${bordeColor} ">
+      <div class="follower__card__content" >
+        <div class="content__info">
+          <div className="info__red-social"><img src="./assets/images/icon-${
+            item.network
+          }.svg" alt="icon facebook"  /></div>
+          <p class="info__name">${item.user}</p>
+        </div>
+        <p class="content__audience">${item.audience}</p>
+        <p class="content__text">${item.audienceType}</p>
+        <div class="content__state">
+          <img src="./assets/images/${nameIcon}" alt="${nameIcon}" class="state__img"/>
+          <p class="state__text ${item.isUp ? "up" : "down"} ">
+          ${item.today} Today</p>
+        </div>
+      </div>
+    </div>`;
+    mainFollower.innerHTML += templateCardFollower;
+  });
+}
+
+function renderOverview(data) {
+  data.map(function (item) {
+    let nameIcon = "";
+
+    if (item.isUp) {
+      nameIcon = "icon-up.svg";
+    } else {
+      nameIcon = "icon-down.svg";
+    }
+    const templateCardOverview = ` 
+    <div class="overview__item">
+      <div class="item__titles">
+        <p class="item__statsType">${item.statsType}</p>
+        <img src="./assets/images/icon-${item.network}.svg" 
+        alt="Icono de ${item.network}" class="item__network" />
+    </div>
+    <div class="items__stacts">
+      <p class="stacts__stact">${item.stats}</p>
+      <div class="stact__container">
+      <img src="./assets/images/${nameIcon}" alt= "Icono de state" class="stact__img" />
+        <p class="porcentage ${item.isUp ? "up" : "down"} ">${item.porcentage}%</p>
+      </div>
+    </div>
+  </div>
+</section>`;
+
+    overviewContainer.innerHTML += templateCardOverview;
+  });
+}
+
+// Función encargada de activar el modo dark
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const inputModoDark = document.getElementById("container-dark-mode__input");
+
+  const root = document.documentElement;
+
+  if (root.getAttribute('data-theme') === "dark") {
+    inputModoDark.checked= true;
+  }
+
+
+  inputModoDark.addEventListener("input", () => {
+    const containerBtn = document.querySelector(".container-dark-mode__btn")
+     inputModoDark.checked ? containerBtn.classList.add("active") : containerBtn.classList.remove("active")
+    toggleModoDark();
+  });
+
+  function toggleModoDark() {
+    const setTheme= inputModoDark.checked ? 'dark' : 'light';
+
+
+    root.setAttribute('data-theme', setTheme);
+    localStorage.setItem('theme', setTheme);
+  }
+});
